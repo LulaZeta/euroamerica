a1bc68946c9f88d9a9c54bb5febcef112c5d61e6
Luciana Zanetti <luciana.zeta@gmail.com>
2023-03-21 12:04:55 -0300

    Initial commit

---
+++ api/src/models/Product.js
@@ -0,0 +1,36 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('product', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    dni: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    patente: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    cuit: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    origen: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    destino: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+};
